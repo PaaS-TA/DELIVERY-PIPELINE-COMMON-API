51b7d1361cd98681326a2c23f8ecf31a05073fe5
hrjin <hyunlee@bluedigm.com>
2018-01-16 16:53:15 +0900

    serviceInstance 가 삭제될 시 삭제되어야 할 항목 삭제 로직 추가.(grantedAuthority, pipeline, sonar_project, cf_info, cf_url)

--- src/main/java/paasta/delivery/pipeline/common/api/domain/common/serviceInstance/ServiceInstancesService.java
+++ src/main/java/paasta/delivery/pipeline/common/api/domain/common/serviceInstance/ServiceInstancesService.java
@@ -1,9 +1,23 @@
 package paasta.delivery.pipeline.common.api.domain.common.serviceInstance;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 import paasta.delivery.pipeline.common.api.common.Constants;
+import paasta.delivery.pipeline.common.api.domain.common.authority.GrantedAuthority;
+import paasta.delivery.pipeline.common.api.domain.common.authority.GrantedAuthorityService;
+import paasta.delivery.pipeline.common.api.domain.common.cf.info.CfInfo;
+import paasta.delivery.pipeline.common.api.domain.common.cf.info.CfInfoRepository;
+import paasta.delivery.pipeline.common.api.domain.common.cf.url.CfUrl;
+import paasta.delivery.pipeline.common.api.domain.common.cf.url.CfUrlRepository;
+import paasta.delivery.pipeline.common.api.domain.common.pipeline.Pipeline;
+import paasta.delivery.pipeline.common.api.domain.common.pipeline.PipelineRepository;
+import paasta.delivery.pipeline.common.api.domain.common.pipeline.PipelineService;
+import paasta.delivery.pipeline.common.api.domain.common.project.Project;
+import paasta.delivery.pipeline.common.api.domain.common.project.ProjectRepository;
+import paasta.delivery.pipeline.common.api.domain.common.project.ProjectService;
 
 import java.util.List;
 
@@ -13,10 +27,14 @@ import java.util.List;
 @Service
 public class ServiceInstancesService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ServiceInstancesService.class);
     private final ServiceInstancesRepository serviceInstancesRepository;
+    private final PipelineRepository pipelineRepository;
+    private final ProjectRepository projectRepository;
+    private final CfInfoRepository cfInfoRepository;
+    private final CfUrlRepository cfUrlRepository;
 
     private static final String SHARED = "Shared";
-
 
     private static final String USED_SERVER = "Y";
 
@@ -24,8 +42,25 @@ public class ServiceInstancesService {
     private CiInfoService ciInfoService;
 
     @Autowired
-    public ServiceInstancesService(ServiceInstancesRepository serviceInstancesRepository) {
+    private PipelineService pipelineService;
+
+    @Autowired
+    private InstanceUseService instanceUseService;
+
+    @Autowired
+    private GrantedAuthorityService grantedAuthorityService;
+
+    @Autowired
+    private ProjectService projectService;
+
+
+    @Autowired
+    public ServiceInstancesService(ServiceInstancesRepository serviceInstancesRepository, PipelineRepository pipelineRepository, ProjectRepository projectRepository, CfInfoRepository cfInfoRepository, CfUrlRepository cfUrlRepository) {
         this.serviceInstancesRepository = serviceInstancesRepository;
+        this.pipelineRepository = pipelineRepository;
+        this.projectRepository = projectRepository;
+        this.cfInfoRepository = cfInfoRepository;
+        this.cfUrlRepository = cfUrlRepository;
     }
 
     public ServiceInstances createInstances(@RequestBody ServiceInstances serviceInstances) {
@@ -56,6 +91,52 @@ public class ServiceInstancesService {
         try {
             ServiceInstances serviceInstances = serviceInstancesRepository.getOne(id);
             String ciServerUrl = serviceInstances.getCiServerUrl();
+
+            // 1.  서비스 인스턴스가 삭제되기 전 관련 Granted_Authority 먼저 삭제.
+            List<InstanceUse> instanceUseList = instanceUseService.findByServiceInstancesId(serviceInstances.getId());
+            for(int i = 0; i < instanceUseList.size(); i++){
+                List<GrantedAuthority> grantedAuthorityList = grantedAuthorityService.findByInstanceUseId(instanceUseList.get(i).getId());
+                LOGGER.info("지워질 grantedAuthority 목록 :::::::::: " + grantedAuthorityList);
+                if(grantedAuthorityList.size() > 0) {
+                    grantedAuthorityService.deleteGrantedAuthorityRows(grantedAuthorityList);
+                }
+            }
+
+            // 1. 해당 서비스 인스턴스 아이디를 가지고 있는 파이프라인 목록을 가져온다.
+            List<Pipeline> deletePipelineList = pipelineRepository.findByServiceInstancesId(serviceInstances.getId());
+            LOGGER.info("지워질 파이프라인 목록 :::::::::: " + deletePipelineList.toString());
+
+            // 2. deletePipelineList 가 존재하면 해당 파이프라인 목록으로 for 문을 작성한다. 이 때 PipelineService 를 이용하여 Pipeline 를 차례대로 삭제한다.
+            if(deletePipelineList.size() > 0) {
+                for (int i = 0; i < deletePipelineList.size(); i++) {
+                    pipelineService.deletePipeline(deletePipelineList.get(i).getId());
+                }
+            }
+
+            // 1. 프로젝트 삭제
+            List<Project> projects = projectRepository.findByserviceInstancesId(id);
+            if(projects.size() > 0) {
+                for (int i = 0; i < projects.size(); i++) {
+                    projectService.deleteProject(projects.get(i));
+                }
+            }
+
+            // 1. cf info 삭제
+            List<CfInfo> cfInfoList = cfInfoRepository.findByServiceInstancesId(id);
+            if(cfInfoList.size() > 0) {
+                for (int i = 0; i < cfInfoList.size(); i++) {
+                    cfInfoRepository.deleteCfInoById(cfInfoList.get(i).getId());
+                }
+            }
+
+            // 1. cf url 삭제
+            List<CfUrl> cfUrlList = cfUrlRepository.findByServiceInstancesId(id);
+            if(cfUrlList.size() > 0) {
+                for (int i = 0; i < cfUrlList.size(); i++) {
+                    cfInfoRepository.delete(cfUrlList.get(i).getId());
+                }
+            }
+
             serviceInstancesRepository.delete(id);
             ciInfoService.recovery(ciServerUrl);
         } catch (Exception e) {
